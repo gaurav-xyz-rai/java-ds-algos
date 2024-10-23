e0eb144b692621d66ad76f19163b6a91fe5a8869
Gaurav Rai <gaurav.x.rai@gmail.com>
2024-10-22 23:19:46 -0700

    Arrays

---
+++ arrays/MidArray.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class MidArray
+{
+    public static void main(String[] args)
+    {
+        int[] array = {1, 2};
+        System.out.println(Arrays.toString(MidArray.middle(array)));
+    }
+
+    public static int[] middle(int[] array) {
+        int[] midArray = new int[array.length - 2];
+        for (int i = 1; i < array.length - 1; i++)
+        {
+            midArray[i-1] = array[i];
+        }
+        return midArray;
+    }
+}
